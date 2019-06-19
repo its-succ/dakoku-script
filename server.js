2701265b9556603b0f62549274a07e5338e9f25e
sizuhiko <k-kishida@esm.co.jp>
2019-06-19 20:39:42 +0900

    GAEからのリクエストに限定するようにバリデーションチェックを追加

--- server.js
+++ server.js
@@ -3,13 +3,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+const validate = require('express-validation');
+const Joi = require('joi');
 
 const dakoku = require('./dakoku');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/', jsonParser, async (req, res) => {
+const validation = {
+  headers: {
+    'x-appengine-queuename': Joi.string().required()
+  },
+  body: {
+    user: Joi.string().required(),
+    password: Joi.string().required(),
+    action: Joi.string().required()
+  }
+};
+
+app.post('/', jsonParser, validate(validation), async (req, res) => {
   console.log('receive request');
   await dakoku(req.body.action, req.body.user, req.body.password);
   console.log('complete request');
